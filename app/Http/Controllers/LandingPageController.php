7fc010f692be1cc1b9e8e73d62bdac06e32667aa
Shawn Weigand <shawnweigand1@gmail.com>
2021-10-22 14:33:53 -0400

    banner work

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -6,12 +6,17 @@ use Inertia\Inertia;
 use Illuminate\Http\Request;
 use App\Models\User;
 
-class LandingPageController extends Controller
+class LandingPageController extends Controller`
 {
     public function index()
     {
         return Inertia::render('LandingPage/LandingPage', [
-            'donors' =>  User::where('donor',true)->get('name')->chunk(4)
+            // 'donors' => array_chunk(User::where('donor',true)->get('name'),4)
+            'donors' =>  User::where('donor',true)->get('name')->toArray()
         ]);
+    }
+
+    public function donorChunk($donors,$num) {
+        return array_chunk($donors,$num);
     }
 }
