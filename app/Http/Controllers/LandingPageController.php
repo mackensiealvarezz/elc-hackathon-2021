ebaa246858be40086e595157fad12d9511af7a5c
Shawn Weigand <shawnweigand1@gmail.com>
2021-10-22 15:46:24 -0400

    banner

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -6,13 +6,13 @@ use Inertia\Inertia;
 use Illuminate\Http\Request;
 use App\Models\User;
 
-class LandingPageController extends Controller`
+class LandingPageController extends Controller
 {
     public function index()
     {
         return Inertia::render('LandingPage/LandingPage', [
             // 'donors' => array_chunk(User::where('donor',true)->get('name'),4)
-            'donors' =>  User::where('donor',true)->get('name')->toArray()
+            'donors' =>  User::where('donor',true)->get('name')[0]
         ]);
     }
 
--- resources/js/Pages/LandingPage/BreastCancer.js
+++ resources/js/Pages/LandingPage/BreastCancer.js
@@ -49,7 +49,7 @@ export default function BreastCancer(props) {
 
     return (
         <section aria-labelledby="cause-heading">
-            <div className="relative px-6 py-32 bg-gray-800 sm:py-24 sm:px-12 lg:px-16">
+            <div className="relative px-6 py-32 bg-gray-800 sm:py-12 sm:px-12 lg:px-16">
                 <div className="absolute inset-0 overflow-hidden">
                     <img
                         src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.wfla.com%2Fwp-content%2Fuploads%2Fsites%2F71%2F2020%2F10%2FBreast-cancer-awareness-ribbon.jpg%3Fw%3D1280&f=1&nofb=1"
@@ -61,9 +61,8 @@ export default function BreastCancer(props) {
                 <Ticker>
                     {({ index }) => (
                         <>
-                            <div className="space-y-10">
-                                <div className="flex space-x-10">
-                                    {/* {tickerText(props.donors)} */}
+                            <div className="space-y-5">
+                                <div className="flex space-x-5">
                                     {/* {props.donors[0].map((donor) => {
 
                                         return <h1 style={{ paddingRight: "2em", opacity: "50%", color: "white" }}>{donor.name}</h1>
@@ -75,11 +74,11 @@ export default function BreastCancer(props) {
                                     <h1 style={{ paddingRight: "2em", opacity: "50%", color: "white" }}>Shawn</h1>
                                     <h1 style={{ paddingRight: "2em", opacity: "50%", color: "white" }}>Mackensie Alvarez</h1>
                                 </div>
-                                <div className="flex space-x-8">
+                                <div className="flex space-x-25">
                                     <h1 style={{ paddingRight: "2em", opacity: "50%", color: "white" }}>Shawn</h1>
                                     <h1 style={{ paddingRight: "2em", opacity: "50%", color: "white" }}>Mackensie Alvarez</h1>
                                 </div>
-                                <div className="flex space-x-12">
+                                <div className="flex space-x-10">
                                     <h1 style={{ paddingRight: "2em", opacity: "50%", color: "white" }}>Shawn</h1>
                                     <h1 style={{ paddingRight: "2em", opacity: "50%", color: "white" }}>Mackensie Alvarez</h1>
                                 </div>
@@ -87,13 +86,14 @@ export default function BreastCancer(props) {
                         </>
                     )}
                 </Ticker>
+                <div className="box-border h-10"/>
                 <div className="relative flex flex-col items-center max-w-3xl mx-auto text-center">
                     <h2 id="cause-heading" className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
                         Help us fight breast cancer
                     </h2>
                     <p className="mt-3 text-xl text-white">
                         Breast cancer is now the most commonly diagnosed cancer worldwide, making the need to help find a cure more urgent than ever.
-                        Let’s rally together to help create a breast cancer-free world.
+                        Become a donor and together we can rally to help create a breast cancer-free world.
                     </p>
                     <a
                         href="/cart"
