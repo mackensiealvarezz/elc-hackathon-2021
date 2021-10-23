c4be45cec33bd915579fc8b51617e43cfedf139a
Mackensie Alvarez <mackensiealvarez@gmail.com>
2021-10-23 11:59:48 -0400

    Added seeder and added Link tag to banner

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,28 +20,27 @@ class UserSeeder extends Seeder
             'name' => 'Michael Smith',
             'email' => 'demo@estee.com',
             'password' => Hash::make('demo'),
-            'pin' => Random::generate(3, '0-9')
         ]);
 
         User::create([
             'name' => 'Shawn Weigand',
             'email' => 'shawn@estee.com',
             'password' => Hash::make('shawn'),
-            'pin' => Random::generate(3, '0-9')
         ]);
 
         User::create([
             'name' => 'Mackensie Alvarez',
             'email' => 'mackensie@estee.com',
             'password' => Hash::make('mackensie'),
-            'pin' => Random::generate(3, '0-9')
         ]);
 
         User::create([
             'name' => 'Kishan Patel',
             'email' => 'kishan@estee.com',
             'password' => Hash::make('kishan'),
-            'pin' => Random::generate(3, '0-9')
         ]);
+
+        User::factory()->count(30)->create();
+
     }
 }
